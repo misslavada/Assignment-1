9a901100ca44cf1458704a82277bbc7448ae5029
misslavada <56700239+misslavada@users.noreply.github.com>
2020-02-06 17:35:47 -0800

    completed files for assignment 1

---
+++ scripts.js
@@ -0,0 +1,166 @@
+// step 1
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMoonth;
+let myLibraryFunction;
+
+// step 2
+10.07
+"Hello"
+false
+null
+
+// step 3
+let x = 24;
+let firstName = "Tiffany"
+
+// step 4
+let firstName;
+let lastName;
+let address;
+let city;
+let state;
+let zipCode;
+let yourAge;
+let referralSource;
+let mayWeContactYou;
+
+//step 5
+let firstName; let isCorrect; firstName = "Tiffany"; isCorrect = true;
+let firstName = "Tiffany"; 
+let firstName; firstName = "Tiffany";
+
+let lastName; let isCorrect; lastName = "Walker"; isCorrect = true;
+let lastName = "Walker";
+let lastName; lastName = "Walker"
+
+let address; let isCorrect; address = "Home"; isCorrect = true;
+let address = "Home"
+let address;address = "Home";
+
+let city; let isCorrect; city = "Carlsbad"; isCorrect = true;
+let city = "Carlsbad";
+let city; city = "Carlsbad";
+
+let state; let isCorrect; state = "CA"; isCorrect = true;
+let state = "CA";
+let state; state = "CA";
+
+let zipCode; let isCorrect; zipCode = 92010; isCorrect = true;
+let zipCode = 92010;
+let zipCode; zipCode = 92010;
+
+let yourAge; let isCorrect; yourAge = 37; isCorrect = true;
+let yourAge = 37;
+let yourAge; yourAge = 37;
+
+let lastName; let isCorrect; lastName = "Walker"; isCorrect = true;
+let referralSource = "Website";
+let referralSource; referallSource = "Website"
+
+let mayWeContactYou; let isCorrect; mayWeContactYou = "Yes"; isCorrect = true;
+let mayWeContactYou = "Yes";
+let mayWeContactYou; mayWeContactYou = "Yes";
+
+//step 6
+let age = 37;
+window.console.log("I am " + age + "years old.");
+
+//step 7
+let x = true;
+window.console.log("The answer is " + x);
+
+let x = 7;
+let y = false;
+window.console.log(x + " is " + y);
+
+//step 8
+// No, needs to have string wrapped with double quotations("").
+
+//step 9
+let someValue = null;
+console.log(someValue);
+
+let someValue;
+console.log(someValue);
+
+//step 10
+typeOf "Hello";
+typeOf 56;
+typeOf true;
+typeOf [1,2,3]
+typeOf newInfo;
+
+//step 11
+alert("Hello, Tiffany Walker, welcome to the JavaScript class!");
+
+//step 11a
+let name = "Tiffany Walker";
+alert("Hello, " + name + ", welcome to the JavaScript class!");
+
+//step 12
+let name = "Tiffany Walker";
+let course = "JavaScript"
+alert("Hello, " + name + ", welcome to the " + course + " class!");
+
+//step 13
+alert("Hello, " + name + "." + "<br>" + "Welcome to the " + course + " class!");
+
+//step 14
+let name = prompt("What is your name?");
+let course = "JavaScript"
+alert("Hello, " + name + "." + "<br>" + "Welcome to the " + course + " class!");
+
+//step 15
+let name = "Tiffany Walker";
+let course = prompt("Which class are you taking?");
+alert("Hello, " + name + "." + "<br>" + "Welcome to the " + course + " class!");
+
+//step 16
+let x = 10;
+let y = 20;
+window.console.log(x + y);
+
+//step 17
+let x = 20;
+x += 20;
+
+//step 18
+let x = 20;
+x *= 5;
+
+//step 19
+let x = 20 % 3;
+x /= 1;
+window.console.log(x)
+
+//step 20
+let x = 1;
+let y = 2;
+window.console.log(x < y);
+
+let x = 1;
+let y = 2;
+window.console.log(x = 1 && y = 2);
+
+//step 21
+let a = 250;
+let b = 630;
+window.console.log(a === b);
+
+let a = 250;
+let b = 630;
+window.console.log(a >= b != true);
+
+//step 22
+new Object = "Widget";
+typeOf Object;
+
+//step 23
+instanceOf 3;
+typeOf Number;
+
+//step 24
+instanceOf "dog";
+typeOf Number;
